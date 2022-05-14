4bc6bd5a9a8e0442b460a0b50ecc69169a328ffd
imAFRAZ <95432416+imAFRAZ@users.noreply.github.com>
2022-05-14 22:46:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var first_name = "Ranbir";
+var second_name = "kapoor";
+console.log(first_name);
+
+console.log(second_name);
+var full_name = first_name.concat(second_name);
+console.log(full_name);
+
+var a = "10"
+var b = "5"
+console.log("a");
+console.log("b");
+var square_root = Math.sqrt("10");
+console.log("square_root");
+
+
